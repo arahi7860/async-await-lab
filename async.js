4ba257a4ab438d6850251d1b28cdfd71dfd4a6f7
Steve VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-12-17 07:39:08 -0700

    Comment out functions

--- async.js
+++ async.js
@@ -12,10 +12,10 @@ function fetchData(country) {
 // Step 2: Rewrite Step 1 using Try/Catch & Async/Await
 // Be sure to comment out your Step 1 function before writing the new function
 
-function fetchData(country) {
-  const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
-  // Write code here.
-}
+// function fetchData(country) {
+//   const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`
+//   // Write code here.
+// }
 // Step 3: Create dynamic HTML
 
 function showCountryData(data) {
@@ -24,9 +24,9 @@ function showCountryData(data) {
 
 // Step 4: Append Country Data
 
-function showCountryData(data) {
-  // Add more code from Step 3
-}
+// function showCountryData(data) {
+//   // Add more code from Step 3
+// }
 
 // Step 5: Dynamically search country using HTML form with eventHandler
 
@@ -34,7 +34,7 @@ function showCountryData(data) {
 
 // Step 6: Remove previous country data
 
-function removeCountry() {
- // write code here
- // Make sure to call this function???
-}
+// function removeCountry() {
+//  // write code here
+//  // Make sure to call this function???
+// }
